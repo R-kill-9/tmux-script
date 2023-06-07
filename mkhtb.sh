cd255a976b023c634141a169d7cd4d78994f6e0c
Ricard Medina Amado <78033608+R-kill-9@users.noreply.github.com>
2023-06-07 12:14:18 +0000

    Update mkhtb.sh

--- mkhtb.sh
+++ mkhtb.sh
@@ -2,6 +2,7 @@
 
 # User inputs: ./mkhtb <directory_name>
 
+
 # Create directories
 if [ ! -d "$1" ]; then
   mkdir "$1"
@@ -12,89 +13,99 @@ else
 fi
 
 
+
 ##############
 # Tmux init  #
 ##############
 
-# New detached tmux session
-tmux new-session -d -s "tmux_$1"
 
-############### AUXILIAR CONFIG ###############
+tmux has-session -t "tmux_$1" 2>/dev/null
 
-# Create a new window and name it
-tmux new-window -n "Auxiliar"
+# If doesn't exists it is created
+if [ $? != 0 ] 
+then
+  
 
-# Split the window vertically
-tmux split-window -h
+  # New detached tmux session
+  tmux new-session -d -s "tmux_$1"
 
-# Split the first pane horizontally
-tmux select-pane -t 1
-tmux split-window -v
+  ############### AUXILIAR CONFIG ###############
 
-# Split the second pane horizontally
-tmux select-pane -t 0
-tmux split-window -v
+  # Create a new window and name it
+  tmux new-window -n "Auxiliar"
+
+  # Split the window vertically
+  tmux split-window -h
+
+  # Split the first pane horizontally
+  tmux select-pane -t 1
+  tmux split-window -v
+
+  # Split the second pane horizontally
+  tmux select-pane -t 0
+  tmux split-window -v
 
 
 
-############### MAIN CONFIG ##############
+  ############### MAIN CONFIG ##############
 
-# Select the Main window
+  # Select the Main window
+  tmux select-window -t 0
+
+  # Rename window 0 to Main
+  tmux rename-window "Main"
+
+  # Split the window vertically
+  tmux split-window -h
+
+  # Split the first pane horizontally
+  tmux select-pane -t 1
+  tmux split-window -v
+
+  # Split the second pane horizontally
+  tmux select-pane -t 0
+  tmux split-window -v
+
+  # Split the third pane horizontally
+  tmux select-pane -t 3
+  tmux split-window -v
+
+
+
+  ############### Create Windows Directories ##############
+
+  # Create a new window and name it
+  cd evidence
+  tmux new-window -n "Evidence"
+  tmux select-window -t 2
+  cd ..
+
+  # Create a new window and name it
+  cd scans
+  tmux new-window -n "Scans"
+  tmux select-window -t 3
+  cd ..
+
+  # Create a new window and name it
+  cd logs
+  tmux new-window -n "Logs"
+  tmux select-window -t 4
+  cd ..
+
+  # Create a new window and name it
+  cd scope
+  tmux new-window -n "Scope"
+  tmux select-window -t 5
+  cd ..
+
+  # Create a new window and name it
+  cd tools
+  tmux new-window -n "Tools"
+  tmux select-window -t 6
+
+  fi
+# Select Main window to start
 tmux select-window -t 0
-
-# Rename window 0 to Main
-tmux rename-window "Main"
-
-# Split the window vertically
-tmux split-window -h
-
-# Split the first pane horizontally
-tmux select-pane -t 1
-tmux split-window -v
-
-# Split the second pane horizontally
-tmux select-pane -t 0
-tmux split-window -v
-
-# Split the third pane horizontally
-tmux select-pane -t 3
-tmux split-window -v
-
-
-
-############### Create Windows Directories ##############
-
-# Create a new window and name it
-cd evidence
-tmux new-window -n "Evidence"
-tmux select-window -t 2
-cd ..
-
-# Create a new window and name it
-cd scans
-tmux new-window -n "Scans"
-tmux select-window -t 3
-cd ..
-
-# Create a new window and name it
-cd logs
-tmux new-window -n "Logs"
-tmux select-window -t 4
-cd ..
-
-# Create a new window and name it
-cd scope
-tmux new-window -n "Scope"
-tmux select-window -t 5
-cd ..
-
-# Create a new window and name it
-cd tools
-tmux new-window -n "Tools"
-tmux select-window -t 6
-
-
-
 
 # Attach tmux session
 tmux attach-session -t "tmux_$1"
